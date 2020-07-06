ecaa3b00b9c79fcfb7aeafab3ab31174765ac0d3
Powerkyria <gitteando@gmail.com>
2020-07-06 06:27:13 +0100

    add task

---
+++ 2.7. Arrays y bucles/TAREA2.7-8 Botones de alarma/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const buttons = document.querySelectorAll('button');
+
+function alarm() {
+	document.body.classList.toggle('red');
+}
+
+// Iteramos sobre todos los botones para añadirles a cada uno un event listener
+for (let i = 0; i < buttons.length; i++) {
+	buttons[i].addEventListener('click', alarm);
+}
