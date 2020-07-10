3cb1162b05f888c0a0b12ea40fb2aa1a75660926
Powerkyria <gitteando@gmail.com>
2020-07-10 08:29:29 +0100

    exercise three ends

--- 2.7. Arrays y bucles/TAREA2.7-7 A story of adalabers/main.js
+++ 2.7. Arrays y bucles/TAREA2.7-7 A story of adalabers/main.js
@@ -51,29 +51,36 @@ function averageAge() {
 console.log(averageAge());
 
 // 3.Una función theYoungest que devuelve el nombre de la adalaber más joven.
-// let youngest
 
-// function theYoungest() {
-// 	for (let i = 0; i < contacts.length; i++) {
-// 		
-// 	}
-// 	return;
-// }
+let youngest = 28; // 28 por ej por ser la edad media.
+let name;
 
-// console.log(theYoungest());
+function theYoungest() {
+	for (let i = 0; i < contacts.length; i++) {
+		if (contacts[i].age < youngest) {
+			youngest = contacts[i].age;
+		}
+	}
 
+	for (let i = 0; i < contacts.length; i++) {
+		if (contacts[i].age === youngest) {
+			name = contacts[i].name;
+		}
+	}
+	return name;
+}
 
+console.log(theYoungest());
 
 // 4.Una función countDesigners que devuelve el número de adalabers que son diseñadoras.
 
- // Creamos variable que indica el número de diseñadores. Empezamos en 0.
+// Creamos variable que indica el número de diseñadores. Empezamos en 0.
 
 let numberOfDesigners = 0;
 
 function countDesigners() {
 	for (let i = 0; i < contacts.length; i++) {
-
-		 // Si el trabajo de una adalaber es diseñadora, incrementa el valor de numberOfDesigners en 1
+		// Si el trabajo de una adalaber es diseñadora, incrementa el valor de numberOfDesigners en 1
 
 		if (contacts[i].job === 'diseñadora') {
 			numberOfDesigners++;
