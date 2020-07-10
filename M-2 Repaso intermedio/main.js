c06ef056e20224ae478834543168aca00fffdb55
Powerkyria <gitteando@gmail.com>
2020-07-10 08:20:43 +0100

    full exercise

--- M-2 Repaso intermedio/main.js
+++ M-2 Repaso intermedio/main.js
@@ -4,11 +4,37 @@ const button = document.querySelector('.btn');
 const body = document.querySelector('body');
 const select = document.querySelector('select');
 const face = document.querySelector('.face');
-const opt1 = document.querySelector('.happy');
-const opt2 = document.querySelector('.sad');
+
+// Función que cambia la cara
 
 function state() {
 	face.innerHTML = select.value;
 }
 
-button.addEventListener('click', state);
+// Función para obtener un número aleatorio hasta 100
+
+function number() {
+	return Math.floor(Math.random() * 101);
+}
+
+// Función de números pares e impares, declarando dentro la
+// función anterior, condiciones y cambio de color de fondo
+
+function parImpar() {
+	const result = number();
+
+	if (result % 2 === 1) {
+		body.classList.add('orange');
+	} else if (result % 2 === 0) {
+		body.classList.remove('orange');
+	}
+	console.log(result);
+}
+
+function allFunctions() {
+	state();
+	parImpar();
+}
+
+button.addEventListener('click', allFunctions);
+
