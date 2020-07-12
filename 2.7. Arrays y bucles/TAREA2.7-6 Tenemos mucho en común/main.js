547878d9da9198df3dfc067c68cdfbf25fb179f6
Powerkyria <gitteando@gmail.com>
2020-07-12 07:54:19 +0100

    shows response on screen

--- 2.7. Arrays y bucles/TAREA2.7-6 Tenemos mucho en común/main.js
+++ 2.7. Arrays y bucles/TAREA2.7-6 Tenemos mucho en común/main.js
@@ -3,16 +3,19 @@
 const button = document.querySelector('.send');
 
 function favorite() {
-
-// Las constantes que recogen el valor del input las declaramos dentro de la funcion
-// porque hasta que no le damos al botón no existen
+	// Las constantes que recogen el valor del input las declaramos dentro de la funcion
+	// porque hasta que no le damos al botón no existen
 
 	const info1 = document.querySelector('.info1').value;
 	const info2 = document.querySelector('.info2').value;
 	const info = [ info1, info2 ];
 
 	for (const data of info) {
-		console.log(`¡A mí también me encantó "${data}"! Tenemos mucho en común, humana.`);
+		let newParagraph = document.createElement('h3');
+
+		newParagraph.innerText = `¡A mí también me encantó "${data}"! Tenemos mucho en común, humana.`;
+
+		document.body.appendChild(newParagraph);
 	}
 }
 button.addEventListener('click', favorite);
