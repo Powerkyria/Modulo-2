14ffdefbff3a08c94be317cf45f9029daf28282c
Powerkyria <gitteando@gmail.com>
2020-07-17 16:23:24 +0100

    fix annotation

--- 2.9. Arrays y bucles 2/TAREA2.9-4 Mi lista de tareas/main.js
+++ 2.9. Arrays y bucles 2/TAREA2.9-4 Mi lista de tareas/main.js
@@ -37,7 +37,7 @@ for (let i = 0; i < tasks.length; i++) {
 	}
 }
 
-//Una vez añadidos los checkbox en el html ya los podemos meter en una constante
+//Una vez añadidos los checkbox en el html ya los podemos meter en una variable.
 
 const inputs = document.querySelectorAll('input');
 
