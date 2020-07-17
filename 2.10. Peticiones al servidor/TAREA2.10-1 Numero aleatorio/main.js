ef489dfd08c2a06890b513da95c480d2ee21c7a9
Powerkyria <gitteando@gmail.com>
2020-07-17 10:46:15 +0100

    add theme ten and first exercise

---
+++ 2.10. Peticiones al servidor/TAREA2.10-1 Numero aleatorio/main.js
@@ -0,0 +1,8 @@
+function getNumber() {
+    fetch("https://api.rand.fun/number/integer")
+      .then(response => response.json())
+      .then(data => {
+        document.querySelector('.js-result').innerHTML = data.result
+      });
+  }
+  document.querySelector('.js-number').addEventListener("click", getNumber);
