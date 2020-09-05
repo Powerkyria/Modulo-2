6c7b9afbb5aa0152abb07e0886645ba0be9308a1
Powerkyria <gitteando@gmail.com>
2020-09-05 05:15:09 +0100

    add task 8

---
+++ 2.11. Peticiones al servidor 2/TAREA 2.11-8/main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const btn = document.querySelector('button');
+const ul = document.querySelector('.list');
+const input = document.getElementById('name');
+
+function search() {
+	if (localStorage.getItem(input.value) !== null) {
+		ul.innerHTML = JSON.parse(localStorage.getItem(input.value));
+
+		console.log('Obtenido de la cache');
+	} else {
+		fetch(`https://swapi.dev/api/people/?search=${input.value}`)
+			.then((response) => response.json())
+			.then((data) => {
+				const characters = data.results;
+				let listContent = '';
+				for (const character of characters) {
+					listContent += `<li>${character.name}. Sex: ${character.gender}</li>`;
+				}
+
+				ul.innerHTML = listContent;
+
+				localStorage.setItem(input.value, JSON.stringify(listContent));
+			});
+
+		console.log('Petición a la API');
+	}
+}
+btn.addEventListener('click', search);
