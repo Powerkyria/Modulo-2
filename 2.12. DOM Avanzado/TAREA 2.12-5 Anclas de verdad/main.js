5bdcecc35d9569d1a9ddcad6e63592dcc9a1c094
Powerkyria <gitteando@gmail.com>
2020-09-08 00:51:37 +0100

    add task 5 Anclas de verdad

---
+++ 2.12. DOM Avanzado/TAREA 2.12-5 Anclas de verdad/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const links = document.querySelectorAll('a');
+
+function getScroll() {
+	for (let i = 0; i < links.length; i++) {
+		console.log(links[i]);
+
+		const position = links[i].getBoundingClientRect();
+		console.log(position);
+
+		const attribute = links[i].getAttribute('data-id');
+		console.log(attribute);
+
+		if (attribute === 'firstSection') {
+			window.scrollTo(43, 25);
+		} else if (attribute === 'secondSection') {
+			window.scrollTo(181, 420);
+		} else if (attribute === 'thirdSection') {
+			window.scrollTo(338, 815);
+		}
+	}
+}
+
+links[i].addEventListener('click', getScroll);
