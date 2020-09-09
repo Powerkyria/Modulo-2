3bcf8c6bc092dc65f9ee62f991e626aa6aae6793
Powerkyria <gitteando@gmail.com>
2020-09-09 18:44:48 +0100

    add image type gift

--- 2.11. Peticiones al servidor 2/TAREA 2.11-10 Dame gifs de gatetes/main.js
+++ 2.11. Peticiones al servidor 2/TAREA 2.11-10 Dame gifs de gatetes/main.js
@@ -3,7 +3,7 @@
 const img = document.querySelector('.picture');
 
 function getCat() {
-	fetch('https://api.thecatapi.com/v1/images/search', {
+	fetch('https://api.thecatapi.com/v1/images/search?mime_types=gif', {
 		headers: {
 			'x-api-key': '75befdf4-72d1-47b7-814f-58e6db172698',
 		},
@@ -11,7 +11,7 @@ function getCat() {
 		.then((response) => response.json())
 		.then((data) => {
 
-			img.src = data.url;
+			img.src = data[0].url;
 			img.alt = 'Un gato';
 		});
 }
