59852b7d172f8d2b7374fd34312e7bba30485764
Powerkyria <gitteando@gmail.com>
2020-09-11 05:37:38 +0100

    add task 6

---
+++ 2.13. Metodos funcionales del array/TAREA 2.13-6 Los usuarios que pueden entrar/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Ya hemos conseguido las contraseñas pertenecientes a cada usuario. ¿Podrías darnos un array con los usuarios que pueden acceder a la aplicación, es decir, los que tienen como PIN un número par?
+
+const users = [
+	{ name: 'María', isPremium: false, pin: 2389 },
+	{ name: 'Lucía', isPremium: true, pin: 2384 },
+	{ name: 'Susana', isPremium: true, pin: 2837 },
+	{ name: 'Rocío', isPremium: false, pin: 5232 },
+	{ name: 'Inmaculada', isPremium: false, pin: 8998 },
+];
+
+const even = users.filter((users) => users.pin % 2 === 0);
+
+console.log(even);
