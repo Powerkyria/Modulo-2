90c71c13377cac6a151485b961574e545cbd5475
Powerkyria <gitteando@gmail.com>
2020-09-11 06:38:37 +0100

    add task 9

---
+++ 2.13. Metodos funcionales del array/TAREA 2.13-9 El ganador de los estudiantes/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Como en el ejemplo anterior vamos a averiguar quién ha ganado usando reduce, pero queremos saber el ganador de los estudiantes, por lo que tendremos que filtrar primero quiénes lo son.
+
+const runners = [
+	{ name: 'Gregory Goyle', time: 56, student: true },
+	{ name: 'Nymphadora Tonks', time: 9, student: false },
+	{ name: 'Luna Lovegood', time: 45, student: true },
+	{ name: 'Cedric Diggory', time: 28, student: true },
+	{ name: 'Cho Chang', time: 35, student: true },
+];
+
+const winnerStudents = runners
+	.filter((runners) => runners.student === true)
+	.reduce((winner, runner) => {
+		if (winner.time < runner.time) {
+			return winner;
+		} else {
+			return runner;
+		}
+	});
+
+console.log(winnerStudents);
