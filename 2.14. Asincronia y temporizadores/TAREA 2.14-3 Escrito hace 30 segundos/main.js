4fafbc918b051d3a2f59d3f82ebeeb2b86e35562
Powerkyria <gitteando@gmail.com>
2020-09-11 23:45:15 +0100

    add comment

--- 2.14. Asincronia y temporizadores/TAREA 2.14-3 Escrito hace 30 segundos/main.js
+++ 2.14. Asincronia y temporizadores/TAREA 2.14-3 Escrito hace 30 segundos/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// Vamos a realizar el típico mensaje que aparece en un blog con la información de hace cuanto se escribió un post. Por ejemplo, con el texto: "escrito hace 30 segundos". Al principio escribiremos en pantalla "escrito hace 1 segundo" e iremos aumentando el número de segundos. Cuando lleve más de 59 segundos queremos que ponga "escrito hace 1 min".
+
 let counter = 0;
 
 const incrementAndShowCounter = () => {
