02cdd6548296747498eea6e3a89bbcdbca2d3822
Powerkyria <gitteando@gmail.com>
2020-09-12 00:31:37 +0100

    add overlay

--- 2.14. Asincronia y temporizadores/TAREA 2.14-4 Su sesión ha expirado/main.js
+++ 2.14. Asincronia y temporizadores/TAREA 2.14-4 Su sesión ha expirado/main.js
@@ -3,12 +3,13 @@
 // Con JavaScript, crear un código para mostrar una ventana en nuestro navegador una vez transcurridos 15 segundos que ponga "su sesión ha expirado" (creada usando HTML y CSS).
 
 const modal = document.querySelector('.modal');
-
+const overlay = document.querySelector('.overlay');
 const message = document.querySelector('p');
 
 const showModal = () => {
 	message.innerHTML = 'Su sesión ha expirado';
 	modal.classList.remove('hidden');
+	overlay.style.opacity = 1;
 };
 
-setTimeout(showModal, 15000);
+setTimeout(showModal, 5000);
