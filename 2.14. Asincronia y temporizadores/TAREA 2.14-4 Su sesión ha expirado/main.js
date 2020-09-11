b9564ede9fc1a59b8a18a264b1a232a759182c17
Powerkyria <gitteando@gmail.com>
2020-09-12 00:15:52 +0100

    add task 4

---
+++ 2.14. Asincronia y temporizadores/TAREA 2.14-4 Su sesión ha expirado/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Con JavaScript, crear un código para mostrar una ventana en nuestro navegador una vez transcurridos 15 segundos que ponga "su sesión ha expirado" (creada usando HTML y CSS).
+
+const modal = document.querySelector('.modal');
+
+const message = document.querySelector('p')
+
+	const showModal = () => {
+		message.innerHTML= 'Su sesión ha expirado'
+		modal.classList.remove('hidden');
+	};
+
+	setTimeout(showModal, 15000);
+
